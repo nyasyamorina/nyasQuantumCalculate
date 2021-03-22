9cbce8ac9142796885ce48585966971fa57c64e6
nyasyamorina <44353997+nyasyamorina@users.noreply.github.com>
2021-03-22 23:33:35 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+import os
+import sys
+
+setup(
+    name = "nyasQuantumCalculate",
+    version = "0.0.1",
+    author = "nyasyamorina",
+    author_email = "1275935966@qq.com",
+    description = "A Simple Quantum Calculation Simulate Packge",
+    # TODO
+    url = "",
+    packages=["nyasQuantumCalculate"],
+)
+
+site_paths = [path for path in sys.path
+              if path.endswith("site-packages") and \
+                  "lib" in path]
+
+package_path = os.path.join(site_paths[0], "nyasQuantumCalculate")
+assert os.path.exists(package_path)
+
+cy_path_src = "nyasQuantumCalculate/cy"
+cy_path_tar = os.path.join(package_path, "cy")
+os.makedirs(cy_path_tar, exist_ok=True)
+for name in os.listdir(cy_path_src):
+    if name.endswith(".pyd") or name.endswith(".pyi"):
+        with open(os.path.join(cy_path_src, name), "rb") as src:
+            data = src.read()
+        with open(os.path.join(cy_path_tar, name), "wb") as tar:
+            tar.write(data)
